b961e4973f3d82093feed5f96ce32b979c299960
ZvezdoeD642 <vanya.gagarin@gmail.com>
2026-07-28 14:38:53 +0100

    Update eigenvectors.py

--- eigenvectors.py
+++ eigenvectors.py
@@ -1,1 +1,12 @@
+import math
+import numpy as np
 
+#creating matrix
+a, b, c, d = 4, 1, 2, 3
+matrix = np.array([[a, c], [b, d]])
+
+#finding eigenvectors
+eigenvalues, eigenvectors = np.linalg.eig(matrix)
+
+print("Eigenvalues:", eigenvalues)
+print("Eigenvectors:", eigenvectors)
